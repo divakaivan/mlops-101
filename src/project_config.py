8d721c3041eaa3ff9edd3e0cb4a7d125c8f4abff
divakaivan <timee98@outlook.com>
2025-02-08 20:01:35 +0900

    update project config class

--- src/project_config.py
+++ src/project_config.py
@@ -1,3 +1,4 @@
+from typing import Optional
 import yaml
 from pydantic import BaseModel
 
@@ -6,10 +7,14 @@ class ProjectConfig(BaseModel):
     """Project configuration"""
 
     gcs_raw_data_bucket_name: str
+    gcs_processed_taxi_data_bucket_name: str
     taxi_data_years: list[int]
     taxi_data_months: list[int]
     taxi_type: str
     green_taxi_raw_schema: list[dict]
+    num_features: list[str]
+    cat_features: Optional[list[str]]
+    target: list[str]
 
     @classmethod
     def from_yaml(cls, config_path: str):
