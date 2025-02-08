c74061dc4794a1b068f3b9e8bd06b85697d02691
divakaivan <timee98@outlook.com>
2025-02-08 14:11:34 +0900

    add project config class

---
+++ src/project_config.py
@@ -0,0 +1,17 @@
+import yaml
+from pydantic import BaseModel
+
+class ProjectConfig(BaseModel):
+    """Project configuration"""
+
+    gcs_raw_data_bucket_name: str
+    taxi_data_years: list[int]
+    taxi_data_months: list[int]
+    taxi_type: str
+
+    @classmethod
+    def from_yaml(cls, config_path: str):
+        """Load configuration from yaml file"""
+        with open(config_path, "r") as f:
+            config_dict = yaml.safe_load(f)
+        return cls(**config_dict)
