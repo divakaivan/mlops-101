6258fbe0b8f5be97d4fe502a1ab357be99878738
divakaivan <timee98@outlook.com>
2025-02-08 20:02:39 +0900

    update data loader to use gcs connector

--- scripts/1_load_data.py
+++ scripts/1_load_data.py
@@ -1,6 +1,7 @@
 import yaml
 import logging
-from make_data.data_loader import NYCTaxiDataFetcher, ParquetDataSaver, GCSUploader
+from make_data.data_loader import NYCTaxiDataFetcher, ParquetDataSaver
+from make_data.gcs_connector import GCSConnector
 from project_config import ProjectConfig
 
 logging.basicConfig(
@@ -10,20 +11,18 @@ logger = logging.getLogger(__name__)
 
 
 config = ProjectConfig.from_yaml("project-config.yaml")
-logger.info("Loaded config: ", yaml.dump(config, default_flow_style=False))
+logger.info(yaml.dump(config, default_flow_style=False))
 
 data_fetcher = NYCTaxiDataFetcher(taxi_type=config.taxi_type)
-gcs_uploader = GCSUploader(bucket_name=config.gcs_raw_data_bucket_name)
+gcs_connector = GCSConnector(bucket_name=config.gcs_raw_data_bucket_name)
 
 for year in config.taxi_data_years:
     for month in config.taxi_data_months:
         data = data_fetcher.fetch(year=year, month=month)
         file_name = f"{config.taxi_type}_tripdata_{year}-{month:02d}.parquet"
-        if gcs_uploader.check_file_exists(file_name):
+        if gcs_connector.check_file_exists(file_name):
             logger.info(f"File {file_name} already exists in GCS bucket. Skipping")
         else:
-            data_saver = ParquetDataSaver(data)
-            data_saver.validate_schema(config.green_taxi_raw_schema)
-            data_saver.save(file_name)
-            gcs_uploader.upload(file_name, f"{file_name}")
-            data_saver.cleanup(file_name)
+            data_checker = ParquetDataSaver(data)
+            data_checker.validate_schema(config.green_taxi_raw_schema)
+            gcs_connector.upload(data, file_name)
