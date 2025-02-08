511e2dd26f7aa5c25fe24a4874538ef91a08c825
divakaivan <timee98@outlook.com>
2025-02-08 14:11:46 +0900

    load data from API to GCS

--- scripts/1_load_data.py
+++ scripts/1_load_data.py
@@ -1,3 +1,26 @@
-from make_data.data_loader import NYCTaxiDataLoader
+import logging
+from make_data.data_loader import NYCTaxiDataFetcher, ParquetDataSaver, GCSUploader
+from project_config import ProjectConfig
 
-print('yay it works!')
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
+
+
+config = ProjectConfig.from_yaml("project-config.yaml")
+
+data_fetcher = NYCTaxiDataFetcher(taxi_type=config.taxi_type)
+data_saver = ParquetDataSaver()
+gcs_uploader = GCSUploader(bucket_name=config.gcs_raw_data_bucket_name)
+
+for year in config.taxi_data_years:
+    for month in config.taxi_data_months:
+        data = data_fetcher.fetch(year=year, month=month)
+        file_name = f"{config.taxi_type}_tripdata_{year}-{month:02d}.parquet"
+        if gcs_uploader.check_file_exists(file_name):
+            logger.info(f"File {file_name} already exists in GCS bucket. Skipping")
+        else:
+            data_saver.save(data, file_name)
+            gcs_uploader.upload(file_name, f"{file_name}")
+            data_saver.cleanup(file_name)
--- src/make_data/data_loader.py
+++ src/make_data/data_loader.py
@@ -1,13 +1,24 @@
+import os
 import pandas as pd
 from google.cloud import storage
 from abc import ABC, abstractmethod
 import requests
 from io import BytesIO
+import logging
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
 
 
 class DataSaver(ABC):
     @abstractmethod
     def save(self, data: pd.DataFrame, file_name: str):
+        pass
+
+    @abstractmethod
+    def cleanup(self, file_name: str):
         pass
 
 
@@ -16,7 +27,7 @@ class NYCTaxiDataFetcher:
 
     def __init__(self, taxi_type: str = "green"):
         """
-        Constructor for fetching NYC Taxi data.
+        Create a class for fetching NYC Taxi data.
 
         Args:
             taxi_type (str, optional): Type of taxi data (e.g., "green", "yellow"). Defaults to "green".
@@ -37,9 +48,6 @@ class NYCTaxiDataFetcher:
             year (int): Year for which to fetch the data (e.g., 2020)
             month (int): Month for which to fetch the data (e.g., 1 for January)
 
-        Raises:
-            ValueError: If the request fails or no data is found.
-
         Returns:
             pd.DataFrame: Pandas DataFrame containing the fetched data.
         """
@@ -47,14 +55,15 @@ class NYCTaxiDataFetcher:
 
         try:
             response = requests.get(url)
-            response.raise_for_status()  
+            response.raise_for_status()
             return pd.read_parquet(BytesIO(response.content))
 
         except requests.exceptions.RequestException as e:
-            raise ValueError(f"Request failed: {e}")
+            logger.error(f"Request failed: {e}")
 
         except pd.errors.EmptyDataError:
-            raise ValueError(f"No data found at {url}")
+            logger.error(f"No data found at {url}")
+
 
 class ParquetDataSaver(DataSaver):
     def save(self, data: pd.DataFrame, file_name: str):
@@ -67,13 +76,26 @@ class ParquetDataSaver(DataSaver):
             file_name (str): Name of the file
         """
         data.to_parquet(file_name, index=False)
-        print(f"Data saved to {file_name}")
+        logging.info(f"Data saved to {file_name}")
+
+    def cleanup(self, file_name: str):
+        """
+        Remove the saved parquet file from local storage.
+
+        Args:
+            file_name (str): Name of the file to remove
+        """
+        if os.path.exists(file_name):
+            os.remove(file_name)
+            logging.info(f"File {file_name} removed successfully.")
+        else:
+            logging.info(f"File {file_name} does not exist.")
 
 
 class GCSUploader:
     def __init__(self, bucket_name: str):
         """
-        (Constructor) Upload data to Google Cloud Storage bucket
+        Create a class to upload data to Google Cloud Storage bucket
 
         Args:
             bucket_name (str): Name of the bucket
@@ -92,4 +114,17 @@ class GCSUploader:
         bucket = self.client.bucket(self.bucket_name)
         blob = bucket.blob(destination)
         blob.upload_from_filename(file_name)
-        print(f"File {file_name} uploaded to gs://{self.bucket_name}/{destination}")
+        logging.info(
+            f"File {file_name} uploaded to gs://{self.bucket_name}/{destination}"
+        )
+
+    def check_file_exists(self, file_name: str):
+        """
+        Check if the file exists in the bucket
+
+        Args:
+            file_name (str): Name of the file to check
+        """
+        bucket = self.client.bucket(self.bucket_name)
+        blob = bucket.blob(file_name)
+        return blob.exists()
