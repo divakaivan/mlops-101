0833a07618927fde686990056db0437f14981003
divakaivan <timee98@outlook.com>
2025-02-09 03:00:29 +0900

    test env reading

--- scripts/3_train_model.py
+++ scripts/3_train_model.py
@@ -12,6 +12,7 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(
 logger = logging.getLogger(__name__)
 
 mlflow.set_tracking_uri(os.getenv("MLFLOW_TRACKING_URI"))
+logging.info(os.getenv("MLFLOW_TRACKING_URI"))
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
