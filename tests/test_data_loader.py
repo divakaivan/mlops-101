2a5bc4f6e425deb8313432cfd519f4ee32b81d6f
divakaivan <timee98@outlook.com>
2025-02-08 14:45:50 +0900

    fix ruff

--- tests/test_data_loader.py
+++ tests/test_data_loader.py
@@ -5,20 +5,23 @@ from io import BytesIO
 import requests_mock
 from make_data.data_loader import NYCTaxiDataFetcher, ParquetDataSaver, GCSUploader
 
+
 @pytest.fixture
 def requests_mock_fixture():
     with requests_mock.Mocker() as m:
         yield m
 
+
 @pytest.fixture
 def sample_data():
     yield pd.DataFrame({"col1": [1, 2, 3]})
+
 
 def test_nyc_taxi_data_fetcher_fetch(requests_mock_fixture, sample_data):
     year, month = 2023, 1
     fetcher = NYCTaxiDataFetcher("green")
     url = fetcher._construct_url(year, month)
-    
+
     buffer = BytesIO()
     sample_data.to_parquet(buffer, index=False)
     buffer.seek(0)
@@ -29,6 +32,7 @@ def test_nyc_taxi_data_fetcher_fetch(requests_mock_fixture, sample_data):
     assert not df.empty
     assert "col1" in df.columns
 
+
 def test_nyc_taxi_data_fetcher_fetch_failure(requests_mock_fixture):
     """Test the case where the fetch fails and status code 1 is returned."""
     fetcher = NYCTaxiDataFetcher("green")
@@ -37,6 +41,7 @@ def test_nyc_taxi_data_fetcher_fetch_failure(requests_mock_fixture):
 
     df = fetcher.fetch(2023, 1)
     assert df == 1
+
 
 def test_parquet_data_saver_save(sample_data):
     saver = ParquetDataSaver()
@@ -48,12 +53,14 @@ def test_parquet_data_saver_save(sample_data):
     loaded_df = pd.read_parquet(file_name)
     assert loaded_df.equals(sample_data)
 
+
 def test_parquet_data_saver_cleanup():
     saver = ParquetDataSaver()
     file_name = "test.parquet"
 
     saver.cleanup(file_name)
     assert not os.path.exists(file_name)
+
 
 def test_gcs_uploader_upload(mocker, tmp_path):
     mock_client = mocker.patch("google.cloud.storage.Client")
@@ -69,6 +76,7 @@ def test_gcs_uploader_upload(mocker, tmp_path):
     uploader.upload(str(file_name), "destination/test.parquet")
 
     mock_blob.upload_from_filename.assert_called_once_with(str(file_name))
+
 
 def test_gcs_uploader_check_file_exists(mocker):
     mock_client = mocker.patch("google.cloud.storage.Client")
