011ce6150471d891c4925b4ff4ba641a3d6084c3
divakaivan <timee98@outlook.com>
2025-02-09 01:56:58 +0900

    update project config

--- scripts/2_process_data.py
+++ scripts/2_process_data.py
@@ -21,7 +21,7 @@ data_processor.process_data()
 train_set, test_set = data_processor.split_data(test_size=0.2, random_state=42)
 
 processed_gcs_bucket_connector = GCSConnector(bucket_name=config.gcs_processed_taxi_data_bucket_name)
-train_file_name = "green_taxi_train_set.parquet"
+train_file_name = config.train_file_name
 processed_gcs_bucket_connector.upload(train_set, train_file_name)
-test_file_name = "green_taxi_test_set.parquet"
+test_file_name = config.test_file_name
 processed_gcs_bucket_connector.upload(test_set, test_file_name)
--- src/project_config.py
+++ src/project_config.py
@@ -16,6 +16,9 @@ class ProjectConfig(BaseModel):
     num_features: list[str]
     cat_features: Optional[list[str]]
     target: list[str]
+    train_file_name: str
+    test_file_name: str
+    experiment_name: str
 
     @classmethod
     def from_yaml(cls, config_path: str):
@@ -23,3 +26,8 @@ class ProjectConfig(BaseModel):
         with open(config_path, "r") as f:
             config_dict = yaml.safe_load(f)
         return cls(**config_dict)
+
+
+class Tags(BaseModel):
+    git_sha: str
+    branch: str
