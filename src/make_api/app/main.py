3251235b7b861c2f065f7edad871a3fc781c67c0
divakaivan <timee98@outlook.com>
2025-02-09 14:08:32 +0900

    update api to read latest mlflow model

--- src/make_api/app/main.py
+++ src/make_api/app/main.py
@@ -1,15 +1,27 @@
 import os
 
 import mlflow
+import pandas as pd
 import uvicorn
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 mlflow.set_tracking_uri(os.getenv("MLFLOW_TRACKING_URI"))
 
 model_uri = "models:/taxi_fare_prediction.taxi_fare_model@latest-model"
-model = mlflow.sklearn.load_model(model_uri)
+pipe = mlflow.sklearn.load_model(model_uri)
 
 app = FastAPI()
+
+
+class InferenceInput(BaseModel):
+    passenger_count: int
+    trip_type: int
+    congestion_surcharge: float
+    mean_distance: float
+    mean_duration: float
+    rush_hour: int
+    vendor_id: str
 
 
 @app.get("/")
@@ -17,9 +29,13 @@ async def read_root():
     return {"Hello": "Visit /docs for API documentation"}
 
 
-@app.get("/predict")
-async def predict():
-    return {"prediction": "1"}
+@app.post("/predict")
+async def predict_one(data: InferenceInput) -> dict[str, float]:
+    df_input = pd.DataFrame([data.dict()])
+
+    prediction = pipe.predict(df_input)
+
+    return {"prediction": prediction[0]}
 
 
 if __name__ == "__main__":
